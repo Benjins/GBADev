be3f65e3726c005769a305a93eb85b7de327311d
Benji Smith <smith.benj@husky.neu.edu>
2016-01-30 16:46:44 -0500

    Fixed DSound demo to loop buffer (doesn't handle cutoff/clipping).  Had to revert asm call to library VBlankIntrWait().

--- dsoundTest/main.c
+++ dsoundTest/main.c
@@ -22,12 +22,6 @@ typedef int int32;
 #define REG_FIFO_A      (*(volatile uint16*)0x040000A0)
 #define REG_FIFO_B      (*(volatile uint16*)0x040000A4)
 
-/*
-REG_DMAxSAD	source	0400:00B0h + 0Ch·x
-REG_DMAxDAD	destination	0400:00B4h + 0Ch·x
-REG_DMAxCNT	control	0400:00B8h + 0Ch·x
-*/
-
 #define REG_DMA0_SRCADDR (*(volatile uint32*)0x40000B0)
 #define REG_DMA0_DSTADDR (*(volatile uint32*)0x40000B4)
 #define REG_DMA0_CNT     (*(volatile uint32*)0x40000B8)
@@ -36,6 +30,9 @@ REG_DMAxCNT	control	0400:00B8h + 0Ch·x
 #define REG_DMA1_DSTADDR (*(volatile uint32*)0x40000C0)
 #define REG_DMA1_CNT     (*(volatile uint32*)0x40000C4)
 
+// DMA flags
+#define HALF_WORD_DMA       0x00000000
+#define DEST_REG_SAME       0x00400000
 #define ENABLE_DMA          0x80000000
 #define WORD_DMA            0x04000000
 #define START_ON_FIFO_EMPTY 0x30000000
@@ -46,61 +43,51 @@ REG_DMAxCNT	control	0400:00B8h + 0Ch·x
 static inline void  enable_sound(){REG_SND_STAT |= 0x80;}
 static inline void disable_sound(){REG_SND_STAT &= ~0x80;}
 
-int8 pSample[1024*20];
-
-
-// DMA channel 1 register definitions
-#define REG_DMA1SAD         *(uint32*)0x40000BC  // source address
-#define REG_DMA1DAD         *(uint32*)0x40000C0  // destination address
-#define REG_DMA1CNT         *(uint32*)0x40000C4  // control register
-
-// DMA flags
-#define WORD_DMA            0x04000000
-#define HALF_WORD_DMA       0x00000000
-#define ENABLE_DMA          0x80000000
-#define START_ON_FIFO_EMPTY 0x30000000
-#define DMA_REPEAT          0x02000000
-#define DEST_REG_SAME       0x00400000
-
-// Timer 0 register definitions
-//#define REG_TM0D            *(uint16*)0x4000100
-//#define REG_TM0CNT          *(uint16*)0x4000102
+int8 pSample[4000];
 
 // Timer flags
 #define TIMER_ENABLED       0x0080
-
-// FIFO address defines
-//#define REG_FIFO_A          0x040000A0
-//#define REG_FIFO_B          0x040000A4
 
 // our Timer interval that we calculated earlier (note that this
 // value depends on our playback frequency and is therefore not set in
 // stone)
 #define TIMER_INTERVAL      (0xFFFF - 761)
 
+#include <gba_console.h>
+#include <gba_video.h>
 #include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_input.h>
+
+#define MEM_IO   0x04000000
+#define MEM_VRAM 0x06000000
+#define FRAME_MEM ((volatile uint16*)MEM_VRAM)
+#define REG_DISPLAY        (*((volatile uint32 *)(MEM_IO)))
 
 int main(void){
 	irqInit();
 	irqEnable(IRQ_VBLANK);
 	
-	int pitch = 10;
-	for(int i = 0; i < 1024*20; i++){
+	REG_DISPLAY = 0x0403;
+	
+	int pitch = 6;
+	for(int i = 0; i < 4000; i++){
 		pSample[i] = (i % pitch) >= (pitch/2) ? 120 : -120;
 		
 		if(i % 100 == 0){
 			pitch++;
-			if(pitch >= 20){
-				pitch = 10;
+			if(pitch >= 10){
+				pitch = 6;
 			}
 		}
 	}
+	
 	
 	enable_sound();
 	
 	REG_SND_DMGCNT = 0x3F;
 	
-	REG_SND_DSCNT = 0x0606;
+	REG_SND_DSCNT = 0x0606 | (1 << 8) | (1 << 9);
 	REG_TM0D = 65536 - (16777216 / 1000);
 	REG_TM0CNT = 0x80;
 	REG_TM1D = 65536 - (16777216 / 1000);
@@ -109,13 +96,50 @@ int main(void){
 	REG_DMA1_DSTADDR = 0x040000A0;
 	REG_DMA1_CNT = 0xB640000E;
 	
-	int8* ptr = (int8*)0x040000A0;
+	
+	for(int i = 0; i < 200; i++){
+		FRAME_MEM[240+i] = 0x2CC5;
+	}
 	
 	int idx = 0;
 	while(1){
-		//asm("swi 0x05");
-		//ptr[idx % 4] = (idx % 100 + 20);
+		VBlankIntrWait();
 		idx++;
+		
+		for(int j = 0; j < 61; j++){
+			FRAME_MEM[j+240*5] = 0;
+		}
+		for(int j = 0; j < idx; j++){
+			FRAME_MEM[j+240*5] = 0x7FFF;
+		}
+		
+		if(idx >= 60){
+			idx = 0;
+			
+			/*
+			pitch = 20;
+			for(int i = 0; i < 4000; i++){
+				pSample[i] = (i % pitch) >= (pitch/2) ? 120 : -120;
+				
+				if(i % 100 == 0){
+					pitch++;
+					if(pitch >= 30){
+						pitch = 20;
+					}
+				}
+			}
+			*/
+			
+			
+			REG_DMA1_CNT = 0;
+			
+			
+			REG_DMA1_SRCADDR = (uint32) pSample;
+			REG_DMA1_DSTADDR = 0x040000A0;
+			REG_DMA1_CNT = 0xB640000E;
+			
+			idx = 0;
+		}
 	}
 	
 	return 0;
