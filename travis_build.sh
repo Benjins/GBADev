bba10fb9b6a83dd8149fdc344ee08e2ca4f47dd2
Benji Smith <smith.benj@husky.neu.edu>
2016-02-23 07:00:35 -0500

    Working on travis build.

--- travis_build.sh
+++ travis_build.sh
@@ -1,9 +1,13 @@
 declare -a projects=("topDown" "actionRpg" "sdkTest")
 
+wd=`pwd`
+echo "export DEVKITPRO=$wd/devkitpro" >> ~/.bashrc
+echo "export DEVKITARM=$wd/devkitpro/devkitARM" >> ~/.bashrc
+echo "export PATH=$PATH:$wd/devkitpro/devkitARM/bin" >> ~/.bashrc
 source ~/.bashrc
 
 for i in "${projects[@]}"
 do
-	./pack-assets.sh "$i"
-	./travis_build_project.sh "$i"
+	. ./pack-assets.sh "$i"
+	. ./build.sh "$i"
 done
