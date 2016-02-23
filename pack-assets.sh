8539e7255edcfcadb887fb8f865a7ee1d3117f08
Benji Smith <smith.benj@husky.neu.edu>
2016-02-23 07:08:37 -0500

    Assets should now be able to be build with travis.

--- pack-assets.sh
+++ pack-assets.sh
@@ -1,7 +1,7 @@
-gcc assets.c -o assets.out
+gcc -std=c99 -Wall assets.c -o assets.out
 ./assets.out $1
-gcc sounds.c -o sounds.out
+gcc -std=c99 -Wall sounds.c -o sounds.out
 ./sounds.out $1
-gcc midi.c -o midi.out
+gcc -std=c99 -Wall midi.c -o midi.out
 ./midi.out $1
 
