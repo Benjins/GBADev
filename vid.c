1564078e0471b7bf1cfd79a9e93e0bf84b24da6a
Benji Smith <smith.benj@husky.neu.edu>
2016-04-01 21:42:15 -0400

    Adding video support.  It's going...meh.

---
+++ vid.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SCREEN_WIDTH  240
+#define SCREEN_HEIGHT 160
+
+char* ReadTextFile(const char* fileName, int* outLength){
+	FILE* file = fopen(fileName, "rb");
+	
+	if(file == 0){
+		*outLength = 0;
+		return 0;
+	}
+	
+	fseek(file, 0, SEEK_END);
+	int fileSize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	
+	char* fileBuffer = (char*)malloc(fileSize+1);
+	fread(fileBuffer, 1, fileSize, file);
+	fclose(file);
+	
+	fileBuffer[fileSize] = '\0';
+	
+	*outLength = fileSize;
+	return fileBuffer;
+}
+
+typedef unsigned short rgb15;
+
+static inline rgb15 RGB15(int r, int g, int b) { return r | (g << 5) | (b << 10); }
+
+typedef struct __attribute((packed))__{
+	short fileTag;
+	int fileSize;
+	short reservedA;
+	short reservedB;
+	int imageDataOffset;
+
+	int headerSize;
+	int imageWidth;
+	int imageHeight;
+	short numColorPlanes;
+	short bitDepth;
+	int compressionMethod;
+	int imageDataSize;
+	int horizontalResolution;
+	int verticalResolution;
+	int numPaletteColors;
+	int numImportantColors;
+} BitMapHeader;
+
+typedef struct{
+	int* data;
+	int width;
+	int height;
+} BitmapData;
+
+BitmapData LoadBMPFile(const char* fileName) {
+	FILE* bmpFile = fopen(fileName, "rb");
+	
+	if(bmpFile == NULL){
+		printf("\n\nError: could not open file '%s'.\n", fileName);
+		BitmapData err = {0};
+		return err;
+	}
+	
+	fseek(bmpFile, 0, SEEK_END);
+	size_t fileSize = ftell(bmpFile);
+	fseek(bmpFile, 0 ,SEEK_SET);
+	
+	unsigned char* fileBuffer = (unsigned char*)malloc(fileSize);
+	fread(fileBuffer, fileSize, 1, bmpFile);
+	fclose(bmpFile);
+	
+	BitMapHeader* bmpInfo = (BitMapHeader*)fileBuffer;
+	int width  = bmpInfo->imageWidth;
+	int height = bmpInfo->imageHeight;
+	int* data = (int*)malloc(4*width*height);
+	
+	unsigned char* fileCursor = fileBuffer + bmpInfo->imageDataOffset;
+	
+	for(int j = 0; j < height; j++){
+		for(int i = 0; i < width; i++){
+			int pixel = fileCursor[0] | (fileCursor[1] << 8) | (fileCursor[2] << 16);
+			fileCursor += 3;
+			data[j*width+i] = pixel;
+		}
+	}
+	
+	BitmapData result = {data, width, height};
+	free(fileBuffer);
+	return result;
+}
+
+BitmapData ResizeBMP(BitmapData img, int width, int height){
+	BitmapData newDat;
+	newDat.width = width;
+	newDat.height = height;
+	newDat.data = (int*)malloc(newDat.width*newDat.height*4);
+	
+	float widthRatio = ((float)img.width)/width;
+	float heightRatio = ((float)img.height)/height;
+	
+	float xVal=0,yVal=0;
+	
+	for(int j = 0; j < newDat.height; j++){
+		for(int i = 0; i < newDat.width; i++){
+			int newDatIdx = (j*newDat.width)+i;
+			int imgIdx = ((int)yVal)*img.width + ((int)xVal);
+			
+			newDat.data[newDatIdx] = img.data[imgIdx];
+			
+			xVal += widthRatio;
+		}
+		xVal = 0;
+		yVal += heightRatio;
+	}
+	
+	return newDat;
+}
+
+rgb15* ResampleBMPTo16Bit(BitmapData data){
+	rgb15* resample = (rgb15*)malloc(data.width*data.height*sizeof(rgb15));
+	
+	for(int i = 0; i < data.width*data.height; i++){
+		int r = (data.data[i] & 0xFF0000) >> 16;
+		int g = (data.data[i] & 0xFF00) >> 8;
+		int b = (data.data[i] & 0xFF);
+		
+		resample[i] = RGB15(r >> 3, g >> 3, b >> 3);
+	}
+	
+	return resample;
+}
+
+void WriteBitmapToAssetHeader(char* varName, int frameNum, BitmapData bmpData, FILE* assetHeaderFile){
+	BitmapData resized = ResizeBMP(bmpData, SCREEN_WIDTH, SCREEN_HEIGHT);
+	
+	rgb15* resampled = ResampleBMPTo16Bit(resized);
+	
+	fprintf(assetHeaderFile, "uint16 %s%d[] __attribute__((section(\".rodata\")))= {\n", varName, frameNum);
+	
+	for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++){
+		int x = i % SCREEN_WIDTH;
+		int y = i / SCREEN_WIDTH;
+		
+		int newY = SCREEN_HEIGHT - y - 1;
+		int newIdx = x + newY*SCREEN_WIDTH;
+		
+		fprintf(assetHeaderFile, "%d,", resampled[newIdx]);
+		if(i % 30 == 0){
+			fprintf(assetHeaderFile, "\n");
+		}
+	}
+	
+	fprintf(assetHeaderFile, "\n};\n");
+}
+
+int main(int argc, char** argv){
+	
+	if (argc < 2){
+		printf("Usage: %s [project directory]\n", argv[0]);
+		return 0;
+	}
+	
+	char vidFileName[256] = {0};
+	snprintf(vidFileName, 256, "%s/vid.txt", argv[1]);
+	
+	int fileLength;
+	char* fileBuffer = ReadTextFile(vidFileName, &fileLength);
+	
+	if(fileBuffer == NULL){
+		printf("Could not find '%s', skipping.\n", vidFileName);
+		return 0;
+	}
+	
+	char vidName[256] = {0};
+	char vidFileFormat[256] = {0};
+	int vidStart = 0;
+	int vidEnd = 0;
+	sscanf(fileBuffer, "%s %s %d %d", vidName, vidFileFormat, &vidStart, &vidEnd);
+	
+	char vidAssetFileName[256] = {0};
+	snprintf(vidAssetFileName, 256, "%s/vid.h", argv[1]);
+	
+	FILE* assetHeaderFile = fopen(vidAssetFileName, "wb");
+	
+	fprintf(assetHeaderFile, "typedef struct{uint16** frames; int frameCount; int width; int height;} Video;\n");
+	
+	for(int i = vidStart; i <= vidEnd; i++){
+		printf("Working on frame %d\n", i);
+		char frameFileName[256] = {0};
+		snprintf(frameFileName, 256, vidFileFormat, i);
+		
+		char frameFullFileName[256] = {0};
+		snprintf(frameFullFileName, 256, "%s/%s", argv[1], frameFileName);
+		
+		BitmapData frameBMP = LoadBMPFile(frameFullFileName);
+		WriteBitmapToAssetHeader(vidName, i, frameBMP, assetHeaderFile);
+	}
+	
+	
+	fprintf(assetHeaderFile, "uint16* %s_frames[] = {\n", vidName);
+	for(int i = vidStart; i <= vidEnd; i++){
+		fprintf(assetHeaderFile, "\t%s%d,\n", vidName, i);
+	}
+	fprintf(assetHeaderFile, "};");
+	
+	fprintf(assetHeaderFile, "Video %s = {%s_frames, %d, %d, %d};\n", vidName, vidName, vidEnd - vidStart + 1, SCREEN_WIDTH, SCREEN_HEIGHT);
+	
+	fclose(assetHeaderFile);
+	
+	return 0;
+}
+
---
+++ vidVid/main.c
@@ -0,0 +1,164 @@
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+typedef char int8;
+typedef short int16;
+typedef int int32;
+
+#define REG_SND_DMGCNT  (*(volatile uint16*)0x4000080)
+#define REG_SND_DSCNT   (*(volatile uint16*)0x4000082)
+#define REG_SND_STAT    (*(volatile uint16*)0x4000084)   
+
+#define REG_TM0D        (*(volatile uint16*)0x4000100)
+#define REG_TM0CNT      (*(volatile uint16*)0x4000102)
+#define REG_TM1D        (*(volatile uint16*)0x4000104)
+#define REG_TM1CNT      (*(volatile uint16*)0x4000106)
+#define REG_TM2D        (*(volatile uint16*)0x4000108)
+#define REG_TM2CNT      (*(volatile uint16*)0x400010A)
+#define REG_TM3D        (*(volatile uint16*)0x400010C)
+#define REG_TM3CNT      (*(volatile uint16*)0x400010E)
+
+#define REG_FIFO_A      (*(volatile uint16*)0x040000A0)
+#define REG_FIFO_B      (*(volatile uint16*)0x040000A4)
+
+#define REG_DMA0_SRCADDR (*(volatile uint32*)0x40000B0)
+#define REG_DMA0_DSTADDR (*(volatile uint32*)0x40000B4)
+#define REG_DMA0_CNT     (*(volatile uint32*)0x40000B8)
+
+#define REG_DMA1_SRCADDR (*(volatile uint32*)0x40000BC)
+#define REG_DMA1_DSTADDR (*(volatile uint32*)0x40000C0)
+#define REG_DMA1_CNT     (*(volatile uint32*)0x40000C4)
+
+#define REG_DMA3_SRCADDR (*(volatile uint32*)0x40000D4)
+#define REG_DMA3_DSTADDR (*(volatile uint32*)0x40000D8)
+#define REG_DMA3_CNT     (*(volatile uint32*)0x40000DC)
+
+// DMA flags
+#define HALF_WORD_DMA       0x00000000
+#define DEST_REG_SAME       0x00400000
+#define ENABLE_DMA          0x80000000
+#define WORD_DMA            0x04000000
+#define START_ON_FIFO_EMPTY 0x30000000
+#define DMA_REPEAT          0x02000000
+
+#define ARRAY_LENGTH(x) (sizeof(x)/sizeof((x)[0]))
+
+static inline void  enable_sound(){REG_SND_STAT |= 0x80;}
+static inline void disable_sound(){REG_SND_STAT &= ~0x80;}
+
+//int8 pSample[304*30];
+
+#define SOUND_BUFFER_SIZE 304
+int8 soundBuffer[SOUND_BUFFER_SIZE*3];
+int8* currBuffer = soundBuffer;
+
+// Timer flags
+#define TIMER_ENABLED       0x0080
+
+#include <gba_interrupt.h>
+
+#define SCREEN_WIDTH  240
+#define SCREEN_HEIGHT 160
+
+#define MEM_IO   0x04000000
+#define MEM_VRAM 0x06000000
+#define FRAME_MEM ((volatile uint16*)MEM_VRAM)
+#define REG_DISPLAY        (*((volatile uint32*)(MEM_IO)))
+#define REG_DISPLAY_VCOUNT (*((volatile uint32 *)(MEM_IO + 0x0006)))
+
+#define REG_DISPLAY_STAT (*((volatile uint16*)(MEM_IO + 0x04)))
+
+/*
+#define REG_IE  (*(volatile uint16*)(MEM_IO + 0x0200))
+#define REG_IF  (*(volatile uint16*)(MEM_IO + 0x0202))
+#define REG_IME (*(volatile uint16*)(MEM_IO + 0x0208))
+*/
+
+//#include "sounds.h"
+//#include "midi.h"
+
+#include "vid.h"
+
+int main(void){
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+	
+	/*
+	uint16 prevIme = REG_IME;
+	REG_IME = 0;
+
+	REG_DISPLAY_STAT = 0x0008;
+	REG_IE = 1;
+
+	REG_IME = prevIme;
+	*/
+	
+	REG_DISPLAY = 0x0403;
+	
+	
+	for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++){
+		FRAME_MEM[i] = 0;
+	}
+	
+	
+	enable_sound();
+	
+	REG_SND_DMGCNT = 0x3F;
+	
+	REG_SND_DSCNT = 0x0606 | (1 << 8) | (1 << 9);
+	REG_TM1D = 65536 - (16777216 / 18157);
+	REG_TM1CNT = 0x80;
+	REG_DMA1_SRCADDR = (uint32) soundBuffer;
+	REG_DMA1_DSTADDR = 0x040000A0;
+	REG_DMA1_CNT = 0xB640000E;
+	
+	
+	int soundCursor = 0;
+	int sample = 0;
+	
+	int frameIdx = 0;
+	
+	while(1){
+		
+		for(int i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; i++){
+			FRAME_MEM[i] = astley.frames[frameIdx][i];
+		}
+		
+		frameIdx++;
+		
+		frameIdx = frameIdx % (astley.frameCount);
+		
+		/*
+		for(int k = 0; k < SOUND_BUFFER_SIZE; k++){
+			currBuffer[k] = snd.data[sample+k];
+		}
+		
+		sample += SOUND_BUFFER_SIZE;
+		
+		if (sample > snd.dataLength){
+			sample -= snd.dataLength;
+		}
+		
+		if(currBuffer == soundBuffer){
+			currBuffer = soundBuffer + SOUND_BUFFER_SIZE;
+			
+			REG_DMA1_CNT = 0;
+			REG_DMA1_CNT = 0xB640000E;
+		}
+		else if(currBuffer == soundBuffer + SOUND_BUFFER_SIZE){
+			currBuffer = soundBuffer + (2*SOUND_BUFFER_SIZE);
+		}
+		else{
+			currBuffer = soundBuffer;
+		}
+		*/
+		
+		while(REG_DISPLAY_VCOUNT != 160);
+		//asm("swi 0x05");
+	}
+	
+	return 0;
+}
+ 
+
